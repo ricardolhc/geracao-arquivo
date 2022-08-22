0a4815b615abe987562f1ac4f521cc4ee2b306a8
Ricardo Costa <ricardo_hentges@hotmail.com>
2022-08-22 18:06:38 -0300

    Adicao da documentacao em HTML utilizando o javadoc

---
+++ Documentacao/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Arquivo"},{"p":"<Unnamed>","l":"Entrevistado"},{"p":"<Unnamed>","l":"EntrevistadoInterface"},{"p":"<Unnamed>","l":"EntrevistadoVetor"}];updateSearchResults();
