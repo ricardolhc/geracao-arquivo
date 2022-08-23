8ab7bb2597545be2f5e3a70724bb6f0e0593dd08
Ricardo Costa <ricardo_hentges@hotmail.com>
2022-08-23 03:37:24 -0300

    Delete Documentacao directory

--- Documentacao/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Arquivo"},{"p":"<Unnamed>","l":"Entrevistado"},{"p":"<Unnamed>","l":"EntrevistadoInterface"},{"p":"<Unnamed>","l":"EntrevistadoVetor"}];updateSearchResults();
